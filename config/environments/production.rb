4f7047daf8754efa904914d55af4c68cbe0e83c7
Max <mrobock@gmail.com>
2017-01-13 12:49:58 -0800

    paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,10 +3,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: ENV.fetch('apartmentimages.bucket'),
-    access_key_id: ENV.fetch('AKIAIUCJYAOYVSOW2IEA'),
-    secret_access_key: ENV.fetch('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'),
-    s3_region: ENV.fetch('us-west-1'),
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
     }
   }
   # Code is not reloaded between requests.
