b7d881d4b20d5647d0b147b7eefbe72834bf96c8
Max <mrobock@gmail.com>
2017-01-29 13:56:48 -0800

    updated styling

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,15 +1,15 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.paperclip_defaults = {
-  storage: :s3,
-  s3_credentials: {
-    bucket: ENV.fetch('apartmentimages.bucket'),
-    access_key_id: ENV.fetch('AKIAIUCJYAOYVSOW2IEA'),
-    secret_access_key: ENV.fetch('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'),
-    s3_region: ENV.fetch('us-west-1'),
-    s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com"
-    }
-  }
+  # config.paperclip_defaults = {
+  # storage: :s3,
+  # s3_credentials: {
+  #   bucket: ENV.fetch('apartmentimages.bucket'),
+  #   access_key_id: ENV.fetch('AKIAIUCJYAOYVSOW2IEA'),
+  #   secret_access_key: ENV.fetch('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'),
+  #   s3_region: ENV.fetch('us-west-1'),
+  #   s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com"
+  #   }
+  # }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,15 +1,15 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.paperclip_defaults = {
-  storage: :s3,
-  s3_credentials: {
-    bucket: ENV.fetch('apartmentimages.bucket'),
-    access_key_id: ENV.fetch('AKIAIUCJYAOYVSOW2IEA'),
-    secret_access_key: ENV.fetch('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'),
-    s3_region: ENV.fetch('us-west-1'),
-    s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com"
-    }
-  }
+  # config.paperclip_defaults = {
+  # storage: :s3,
+  # s3_credentials: {
+  #   bucket: ENV.fetch('apartmentimages.bucket'),
+  #   access_key_id: ENV.fetch('AKIAIUCJYAOYVSOW2IEA'),
+  #   secret_access_key: ENV.fetch('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'),
+  #   s3_region: ENV.fetch('us-west-1'),
+  #   s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com"
+  #   }
+  # }
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
